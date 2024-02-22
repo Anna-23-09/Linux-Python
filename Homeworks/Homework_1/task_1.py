6b6caaa53d1319f246569c0888197e6cdc06a576
Anna <tolstovaaniuta@yandex.ru>
2024-02-22 10:45:09 +0300

    update task_1

--- Homeworks/Homework_1/task_1.py
+++ Homeworks/Homework_1/task_1.py
@@ -7,18 +7,18 @@
 import subprocess
 
 
-def simple_def(command: str, text: str):
+def simple_def(command: str, text: str):    # Принимаем на вход команду и текст, который будем искать в выводе команды
     res = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-    out = res.stdout.lower()
+    out = res.stdout.lower()    # вывод команды помещаем в переменную out
 
-    if text in out and not res.returncode:
+    if text in out and not res.returncode:  # проверяем, есть ли text в выводе команды (out) и что вывод != не 0
         return True
     else:
         return False
 
 
-a = "cat /etc/os-release"
-b = "url"
-c = "cat"
+a = "cat /etc/os-release"   # команда
+b = "url"   # такая строка в выводе есть, ожидаем True
+c = "cat"   # такой строки нет, ожидаем False
 print(simple_def(a, b))
 print(simple_def(a, c))
